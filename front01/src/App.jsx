2926d9819430b8b64266a4b8a085ed44e092f82c
cjo3o <cjy55973743@gmail.com>
2025-05-23 11:13:03 +0900

    no message

--- front01/src/App.jsx
+++ front01/src/App.jsx
@@ -5,13 +5,19 @@ import dbusers from "./users.json";
 
 function App() {
     // const [users, setUsers] = useState([{'name': '홍길동', 'password': '1234'}]);
-    const [users, setUsers] = useState(dbusers);
-    console.log(users);
+    const [users, setUsers] = useState(null);
+    const [supausers, setSupaUsers] = useState(null);
     const getUsers = async (event) => {
-        const result = await axios.get('https://port-0-back01-manacntt27a3e0c2.sel4.cloudtype.app/')
+        const result = await axios.get('http://localhost:8080/')
         const {data, status} = result;
 
         setUsers(data);
+    }
+
+    const getSupaUsers = async (event) => {
+        const {data} = await axios.get('http://localhost:8080/supauser')
+
+        setSupaUsers(data);
     }
 
     return (
@@ -20,9 +26,9 @@ function App() {
                 안녕
             </div>
             {
-                users.map((user) => {
+                users && users.map((user) => {
                     return (
-                        <div key={user.id}>
+                        <div key={user.idx}>
                             <div>{user.id}</div>
                             <div>{user.password}</div>
                         </div>
@@ -31,8 +37,26 @@ function App() {
             }
             <button
                 className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition-all duration-300 ease-in-out transform hover:-translate-y-0.5 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
-                onClick={(event) => getUsers(event)}>불러오기
+                onClick={(event) => getUsers(event)}>
+                mariadb불러오기
             </button>
+            <button
+                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition-all duration-300 ease-in-out transform hover:-translate-y-0.5 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
+                onClick={(event) => getSupaUsers(event)}>
+                supadb불러오기
+            </button>
+            {
+                supausers && supausers.map((supauser) => {
+                    return (
+                        <div key={supauser.id}>
+                            <div>{supauser.name}</div>
+                            <div>{supauser.email}</div>
+                            <div>{supauser.password}</div>
+                            <div>{supauser.created_at}</div>
+                        </div>
+                    )
+                })
+            }
         </>
     )
 }
